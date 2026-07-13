a4c3dd44140671e5b834009c089281cefeda34a2
ishwarii30 <kapadnisishwari30@gmail.com>
2026-07-13 23:25:05 +0530

    best time to buy stock 121

---
+++ Array Problems/Besttimetobuystock121.java
@@ -0,0 +1,17 @@
+class Besttimetobuystock121 {
+    public int maxProfit(int[] prices) {
+       int minPrice= Integer.MAX_VALUE;
+       int maxProfit=0;
+       for(int i=0; i<prices.length; i++ ){
+        if(prices[i]<minPrice){
+            minPrice= prices[i];
+        }
+        int currentProfit= prices[i]-minPrice;
+
+        if(currentProfit>maxProfit){
+            maxProfit= currentProfit;
+        }
+    }
+       return maxProfit;
+}
+}
