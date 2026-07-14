19f78925a428cce735921bd077501e7e738c10c5
ishwarii30 <kapadnisishwari30@gmail.com>
2026-07-14 23:41:27 +0530

    Equal and divisible pairs 2176

---
+++ Array Problems/EqualandDivisiblePairs2176.java
@@ -0,0 +1,13 @@
+class EqualandDivisiblePairs2176 {
+    public int countPairs(int[] nums, int k) {
+        int countpairs=0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]==nums[j] && (i*j)%k==0){
+                    countpairs++;
+                }
+            }
+        }
+        return countpairs;
+    }
+}
